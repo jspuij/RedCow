af36605f3d2453ff3b1c945761c5197837869e9a
Jan-Willem Spuij <jws@birdseyeview.nl>
2020-05-01 16:43:21 +0200

    Implemented clone provider.

---
+++ src/RedCow.Immutable/ICloneProvider.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="ICloneProvider.cs" company="Jan-Willem Spuij">
+// Copyright 2020 Jan-Willem Spuij
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+// modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+
+namespace RedCow
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Text;
+
+    /// <summary>
+    /// Interface for Clone Providers. Clone Providers can clone the public properties of an object
+    /// to another object.
+    /// </summary>
+    public interface ICloneProvider
+    {
+        /// <summary>
+        /// Clones the public properties of an object to another object.
+        /// </summary>
+        /// <typeparam name="TSource">The source type.</typeparam>
+        /// <typeparam name="TDestination">The destination type.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="destination">The destination.</param>
+        public void Clone<TSource, TDestination>(TSource source, TDestination destination);
+    }
+}
